d84f516f4aea71c4fa160ecfce52933e2acbac5b
Christen <timmychris09@gmail.com>
2025-02-22 02:35:20 +0100

    new designs

---
+++ lib/resources/colors.dart
@@ -0,0 +1,7 @@
+
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+Color primaryColor = const Color(0xffECA014);
+}
---
+++ lib/utils/custom_button.dart
@@ -0,0 +1,64 @@
+import 'package:echodate/resources/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+// ignore: must_be_immutable
+class CustomButton extends StatelessWidget {
+  String? text;
+  Color? bgColor;
+  Color? textColor;
+  final VoidCallback ontap;
+  BoxBorder? border;
+  BorderRadiusGeometry? borderRadius;
+  double? height;
+  double? width;
+  Widget? child;
+  CustomButton({
+    super.key,
+    this.text,
+    this.bgColor,
+    required this.ontap,
+    this.textColor,
+    this.border,
+    this.borderRadius,
+    this.height,
+    this.width,
+    this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: ontap,
+      child: Container(
+        height: height ?? 55,
+        alignment: Alignment.center,
+        width: width ?? Get.width,
+        decoration: BoxDecoration(
+          border: border,
+          borderRadius: borderRadius ?? BorderRadius.circular(15),
+          color: bgColor,
+          gradient: bgColor == null
+              ? LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    AppColors().primaryColor,
+                    Color(0xffC18618),
+                  ],
+                )
+              : null,
+        ),
+        child: child ??
+            Text(
+              text.toString(),
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 13,
+                color: textColor ?? Colors.white,
+              ),
+            ),
+      ),
+    );
+  }
+}
