0d7dfc035d35c73f2537d4d56f61de9fea6e57a3
Christentimmy <timmychris09@gmail.com>
2025-07-14 06:29:21 -0700

    interested_in_screen:new-dark-mode

--- lib/app/modules/gender/views/gender_screen.dart
+++ lib/app/modules/gender/views/gender_screen.dart
@@ -1,6 +1,5 @@
 import 'package:echodate/app/controller/user_controller.dart';
 import 'package:echodate/app/modules/gender/widgets/gender_widget.dart';
-import 'package:echodate/app/resources/colors.dart';
 import 'package:echodate/app/widget/custom_button.dart';
 import 'package:echodate/app/widget/loader.dart';
 import 'package:echodate/app/widget/snack_bar.dart';
@@ -21,7 +20,7 @@ class _GenderSelectionScreenState extends State<GenderSelectionScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Get.theme.scaffoldBackgroundColor,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(
@@ -31,49 +30,35 @@ class _GenderSelectionScreenState extends State<GenderSelectionScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  InkWell(
-                    onTap: () {},
-                    child: Container(
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.transparent,
-                        border: Border.all(
-                          width: 1,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      child: const Icon(Icons.arrow_back, color: Colors.black),
+              InkWell(
+                onTap: () {
+                  Get.back();
+                },
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.transparent,
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.grey,
                     ),
                   ),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Skip",
-                      style: TextStyle(
-                        color: AppColors.primaryColor,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    size: 18,
+                    color: Get.theme.primaryColor,
                   ),
-                ],
+                ),
               ),
 
               SizedBox(height: Get.height * 0.1),
 
               // Title
-              const Text(
+              Text(
                 "I am a",
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
+                style: Get.textTheme.headlineMedium,
               ),
 
               const SizedBox(height: 30),
--- lib/app/modules/gender/widgets/gender_widget.dart
+++ lib/app/modules/gender/widgets/gender_widget.dart
@@ -2,11 +2,12 @@ import 'package:echodate/app/resources/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-
-
-Widget buildGenderOption(String gender, RxString selectedGender,{bool showCheck = true}) {
+Widget buildGenderOption(String gender, RxString selectedGender,
+    {bool showCheck = true}) {
   return Obx(() {
     bool isSelected = selectedGender.value == gender;
+    final isDark = Get.isDarkMode;
+
     return GestureDetector(
       onTap: () {
         selectedGender.value = gender;
@@ -18,10 +19,14 @@ Widget buildGenderOption(String gender, RxString selectedGender,{bool showCheck 
           horizontal: 20,
         ),
         decoration: BoxDecoration(
-          color: isSelected ? AppColors.primaryColor : Colors.white,
+          color: isSelected
+              ? AppColors.primaryColor
+              : (isDark ? Colors.grey[900] : Colors.white),
           borderRadius: BorderRadius.circular(10),
           border: Border.all(
-            color: isSelected ? AppColors.primaryColor : Colors.grey.shade300,
+            color: isSelected
+                ? AppColors.primaryColor
+                : (isDark ? Colors.grey[700]! : Colors.grey.shade300),
           ),
         ),
         child: Row(
@@ -32,13 +37,19 @@ Widget buildGenderOption(String gender, RxString selectedGender,{bool showCheck 
               style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.w700,
-                color: isSelected ? Colors.white : Colors.black,
+                color: isSelected
+                    ? Colors.white
+                    : (isDark ? Colors.white : Colors.black),
               ),
             ),
             if (isSelected && showCheck)
               const Icon(Icons.check, color: Colors.white),
             if (!isSelected && !showCheck)
-              const Icon(Icons.arrow_forward_ios, color: Colors.grey, size: 16),
+              Icon(
+                Icons.arrow_forward_ios,
+                color: isDark ? Colors.white54 : Colors.grey,
+                size: 16,
+              ),
           ],
         ),
       ),
